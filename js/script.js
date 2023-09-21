29eea105fd67c58bd9377f05146fc479f891861c
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-21 14:40:43 -0400

    Add addNumberToScreen function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 const prevOp = document.querySelector('.prev-op');
 const newOp = document.querySelector('.new-op');
-const useValueBtns = document.querySelectorAll('.use-value');
+const numBtns = document.querySelectorAll('.num');
+const operatorBtns = document.querySelectorAll('.op');
+const decimalBtn = document.querySelector('.decimal');
 const clearBtn = document.querySelector('.clear');
 const backBtn = document.querySelector('.backspace');
 const signBtn = document.querySelector('.sign');
 const sqrtBtn = document.querySelector('.sqrt');
 const powBtn = document.querySelector('.pow');
 const equalsBtn = document.querySelector('.equals');
+
+function addNumberToScreen(){
+    if (newOp.textContent === '0'){
+        newOp.textContent = this.textContent;
+    } else {
+        if (newOp.textContent.length >= 21){
+            alert("Maxinum numbers reached")
+        } else {
+            newOp.textContent += this.textContent;
+        }
+    }
+}
+
+numBtns.forEach((button) => {
+    button.addEventListener('click', addNumberToScreen);
+});
