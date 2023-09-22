b59919d0f460959766a8dc82ff8d8e5f7c5424e2
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-22 14:02:05 -0400

    Refactor code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const prevOp = document.querySelector('.prev-op');
 const newOp = document.querySelector('.new-op');
 
-let isPlaceHolder = false;
+let isPlaceholder = false;
 let firstNumber = NaN;
 let secondNumber = NaN;
 let operator = null;
+
+const SYMBOL_PLUS = '+';
+const SYMBOL_MINUS = '-';
+const SYMBOL_MULTIPLY = '\u00D7';
+const SYMBOL_DIVIDE = '\u00F7';
+const SYMBOL_MOD = '%';
+const SYMBOL_POW = '^';
 
 function updatePrevOpText(newText){
     prevOp.textContent = newText;
@@ -14,8 +21,15 @@ function updateNewOpText(newText){
     newOp.textContent = newText;
 }
 
+function placeholderCheck(){
+    if (isPlaceholder) {
+        updateNewOpText('0');
+        isPlaceholder = false;
+    }
+}
+
 function nullifyValues(){
-    isPlaceHolder = false;
+    isPlaceholder = false;
     firstNumber = NaN;
     secondNumber = NaN;
     operator = null;
@@ -28,8 +42,7 @@ function onClearClick(){
 }
 
 function isDivideByZero(op, denom){
-    console.log(`Operator: ${op} - Denom: ${denom}`);
-    if ((op === '\u00F7' || op === '%') && denom == 0){
+    if ((op === SYMBOL_DIVIDE || op === SYMBOL_MOD) && denom == 0){
         alert("Cannot divide by zero!")
         secondNumber = NaN;
         return true;
@@ -38,54 +51,46 @@ function isDivideByZero(op, denom){
 }
 
 function onNumberClick(num){
-    if (isPlaceHolder) {
-        updateNewOpText('0');
-        isPlaceHolder = false;
-    }
+    placeholderCheck();
 
     if (newOp.textContent === '0'){
         updateNewOpText(num);
+    } else if (newOp.textContent.length >= 16){
+        alert("Maxinum numbers reached")
     } else {
-        if (newOp.textContent.length >= 21){
-            alert("Maxinum numbers reached")
-        } else {
-            updateNewOpText(newOp.textContent + num);
-        }
+        updateNewOpText(`${newOp.textContent}${num}`);
     }
 }
 
 function onDecimalClick(){
-    if (isPlaceHolder) {
-        updateNewOpText('0');
-        isPlaceHolder = false;
-    }
     if (newOp.textContent.includes('.')) {
         alert("You cannot have more than one decimal!");
     } else {
-        updateNewOpText(newOp.textContent + '.');
+        updateNewOpText(`${newOp.textContent}.`);
+        isPlaceholder = false;
     }
 }
 
 function onSignClick(){
     if (newOp.textContent == 0) return;
 
-    if (newOp.textContent[0] === '-'){
+    if (newOp.textContent[0] === SYMBOL_MINUS){
         updateNewOpText(newOp.textContent.slice(1));
     } else {
-        updateNewOpText('-' + newOp.textContent);
+        updateNewOpText(`-${newOp.textContent}`);
     }
 }
 
 function onBackspaceClick(){
     if (newOp.textContent === '0') return;
 
-    isPlaceHolder = false;
-    let backspacedText = newOp.textContent.slice(0, -1);
+    isPlaceholder = false;
+    const backspacedText = newOp.textContent.slice(0, -1);
     let newText;
 
-    if(newOp.textContent.length === 1 || backspacedText === '-') {
+    if(newOp.textContent.length === 1 || backspacedText === SYMBOL_MINUS) {
         newText = '0';
-    } else if (backspacedText == 0 && backspacedText[0] === '-'){
+    } else if (backspacedText == 0 && backspacedText[0] === SYMBOL_MINUS) {
         newText = backspacedText.slice(1);
     } else {
         newText = backspacedText;
@@ -95,88 +100,86 @@ function onBackspaceClick(){
 }
 
 function onSqrtClick(){
-    // add check for negative number
+    const num = parseFloat(newOp.textContent);
+
+    if (num < 0) {
+        alert("Cannot use \u221Ax with negative numbers!");
+        return;
+    }
+
+    const result = Math.sqrt(num);
+
     if (isNaN(firstNumber)){
-        updatePrevOpText(`\u221A ${newOp.textContent} =`);
-        updateNewOpText(Math.sqrt(parseFloat(newOp.textContent)));
-        isPlaceHolder = true;
+        updatePrevOpText(`\u221A${num} =`);
+        updateNewOpText(result);
     } else {
-        updatePrevOpText(`${firstNumber} ${operator} \u221A ${newOp.textContent} =`);
-        updateNewOpText(firstNumber + Math.sqrt(parseFloat(newOp.textContent)));
+        updatePrevOpText(`${firstNumber} ${operator} \u221A${num} =`);
+        updateNewOpText(operation(firstNumber, result, operator));
         nullifyValues()
     }
-}
-
-function onPowerClick(){
-
+    isPlaceholder = true;
 }
 
 function onOperatorClick(op){
-    let result = 0;
     if (isNaN(firstNumber)){
         firstNumber = parseFloat(newOp.textContent);
         operator = op;
-        isPlaceHolder = true;
         updatePrevOpText(`${firstNumber} ${operator}`);
     } else if (isNaN(secondNumber)) {
         secondNumber = parseFloat(newOp.textContent);
 
         if (isDivideByZero(operator, secondNumber)) return;
-        console.log("No divide by zero");
 
-        if (operator === null){
-            console.log('Second number with null op');
-            operator = op;
-            result = operation(firstNumber, secondNumber, operator);
-            updatePrevOpText(`${firstNumber} ${operator} ${secondNumber} =`);
-        } else {
-            result = operation(firstNumber, secondNumber, operator);
-            operator = op;
-            updatePrevOpText(`${result} ${operator}`);
-        }        
+        const result = operation(firstNumber, secondNumber, operator);
+        operator = op;
+        updatePrevOpText(`${result} ${operator}`);      
         firstNumber = result;
         secondNumber = NaN;
-        isPlaceHolder = true;
     }
+    isPlaceholder = true;
     updateNewOpText('0');
 }
 
 function onEqualsClick(){
     if (isNaN(firstNumber)){
         updatePrevOpText(`${parseFloat(newOp.textContent)} =`);
-        isPlaceHolder = true;
     } else if (isNaN(secondNumber) && operator !== null) {
         secondNumber = parseFloat(newOp.textContent);
+
+        if (isDivideByZero(operator, secondNumber)) return;
+        
         const result = operation(firstNumber, secondNumber, operator);
         updatePrevOpText(`${firstNumber} ${operator} ${secondNumber} =`);
         updateNewOpText(result);
         nullifyValues()
     } 
+    isPlaceholder = true;
 }
 
 function operation(firstNum, secondNum, op){
     let result = 0;
     switch (op) {
-        case '+':
+        case SYMBOL_PLUS:
             result = firstNum + secondNum;
             break;
-        case '-':
+        case SYMBOL_MINUS:
             result = firstNum - secondNum;
             break;
-        case '\u00D7':
+        case SYMBOL_MULTIPLY:
             result = firstNum * secondNum;
             break;
-        case '\u00F7':
+        case SYMBOL_DIVIDE:
             result = firstNum / secondNum;
             break;
-        case '%':
+        case SYMBOL_MOD:
             result = firstNum % secondNum;
+            break;
+        case SYMBOL_POW:
+            result = firstNum ** secondNum;
             break;
         default:
             break;
     }
-    isPlaceHolder = true;
+    isPlaceholder = true;
     return result;
 }
-
-
