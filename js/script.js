5368bde5ea90fa0d0d5d38b0cb5f2a3e991b7c97
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-22 10:02:01 -0400

    Fixed equals and operator button bugs

--- js/script.js
+++ js/script.js
@@ -15,32 +15,40 @@ let firstNumber = NaN;
 let secondNumber = NaN;
 let operator = null;
 
-function addNumberToScreen(){
+function updatePrevOpText(newText){
+    prevOp.textContent = newText;
+}
+
+function updateNewOpText(newText){
+    newOp.textContent = newText;
+}
+
+function onNumberClick(){
     if (isPlaceHolder) {
-        newOp.textContent = 0;
+        updateNewOpText('0');
         isPlaceHolder = false;
     }
 
     if (newOp.textContent === '0'){
-        newOp.textContent = this.textContent;
+        updateNewOpText(this.textContent);
     } else {
         if (newOp.textContent.length >= 21){
             alert("Maxinum numbers reached")
         } else {
-            newOp.textContent += this.textContent;
+            updateNewOpText(newOp.textContent + this.textContent);
         }
     }
 }
 
-function addDecimalToScreen(){
+function onDecimalClick(){
     if (isPlaceHolder) {
-        newOp.textContent = 0;
+        updateNewOpText('0');
         isPlaceHolder = false;
     }
     if (newOp.textContent.includes('.')) {
         alert("You cannot have more than one decimal!");
     } else {
-        newOp.textContent += this.textContent;
+        updateNewOpText(newOp.textContent + this.textContent);
     }
 }
 
@@ -48,76 +56,76 @@ function addSignToScreen(){
     if (newOp.textContent == 0) return;
 
     if (newOp.textContent[0] === '-'){
-        newOp.textContent = newOp.textContent.slice(1);
+        updateNewOpText(newOp.textContent.slice(1));
     } else {
-        newOp.textContent = '-' + newOp.textContent;
+        updateNewOpText('-' + newOp.textContent);
     }
 }
 
 function removeLastNumber(){
-    isPlaceHolder = false;
-    newText = newOp.textContent.slice(0, -1);
     if (newOp.textContent === '0') return;
-    if(newOp.textContent.length === 1 || newText === '-') {
-        newOp.textContent = 0;
-        return;
+
+    isPlaceHolder = false;
+    let backspacedText = newOp.textContent.slice(0, -1);
+    let newText;
+
+    if(newOp.textContent.length === 1 || backspacedText === '-') {
+        newText = '0';
+    } else if (backspacedText == 0 && backspacedText[0] === '-'){
+        newText = backspacedText.slice(1);
+    } else {
+        newText = backspacedText;
     }
-    if (newText == 0 && newText[0] === '-'){
-        newOp.textContent = newText.slice(1);
-        return;
-    }
-    newOp.textContent = newText;
+
+    updateNewOpText(newText)    
 }
 
 function getSquareRoot(){
-    prevOp.textContent = '\u221A' + newOp.textContent + ' ='
-    newOp.textContent = Math.sqrt(parseFloat(newOp.textContent));
+    updatePrevOpText(`\u221A ${newOp.textContent} =`);
+    firstNumber = NaN;
+    secondNumber = NaN;
+    updateNewOpText(Math.sqrt(parseFloat(newOp.textContent)));
     isPlaceHolder = true;
 }
 
 function operatorClicked(){
+    let result = 0;
     if (isNaN(firstNumber)){
-        firstNumber = newOp.textContent.includes('.') ? 
-            parseFloat(newOp.textContent) : parseInt(newOp.textContent);
+        firstNumber = parseFloat(newOp.textContent);
         operator = this.textContent;
-        prevOp.textContent = `${firstNumber} ${this.textContent}`;
         isPlaceHolder = true;
-    } else if (isNaN(secondNumber) && operator === null) {
-        secondNumber = newOp.textContent.includes('.') ? 
-            parseFloat(newOp.textContent) : parseInt(newOp.textContent);
-        operator = this.textContent;
-        result = operation(firstNumber, secondNumber, operator);
-        prevOp.textContent = `${result} ${this.textContent}`;
+        updatePrevOpText(`${firstNumber} ${operator}`);
+    } else if (isNaN(secondNumber)) {
+        secondNumber = parseFloat(newOp.textContent);
+        if (operator === null){
+            console.log('Second number with null op');
+            operator = this.textContent;
+            result = operation(firstNumber, secondNumber, operator);
+            updatePrevOpText(`${firstNumber} ${operator} ${secondNumber} =`);
+        } else {
+            result = operation(firstNumber, secondNumber, operator);
+            operator = this.textContent;
+            updatePrevOpText(`${result} ${operator}`);
+        }        
         firstNumber = result;
         secondNumber = NaN;
         isPlaceHolder = true;
-    } else if (isNaN(secondNumber) && operator !== null) {
-        secondNumber = newOp.textContent.includes('.') ? 
-            parseFloat(newOp.textContent) : parseInt(newOp.textContent);
-        result = operation(firstNumber, secondNumber, operator);
-        operator = this.textContent;
-        prevOp.textContent = `${result} ${this.textContent}`;
-        firstNumber = result;
-        secondNumber = NaN;
-        isPlaceHolder = true;
-    } 
+    }
+    updateNewOpText('0');
 }
 
 function equalsClick(){
     if (isNaN(firstNumber)){
-        firstNumber = newOp.textContent.includes('.') ? 
-            parseFloat(newOp.textContent) : parseInt(newOp.textContent);
-        prevOp.textContent = `${firstNumber} ${this.textContent}`;
+        updatePrevOpText(`${parseFloat(newOp.textContent)} =`);
         isPlaceHolder = true;
     } else if (isNaN(secondNumber) && operator !== null) {
-        secondNumber = newOp.textContent.includes('.') ? 
-            parseFloat(newOp.textContent) : parseInt(newOp.textContent);
-        result = operation(firstNumber, secondNumber, operator);
-        prevOp.textContent = `${firstNumber} ${operator} ${secondNumber} ${this.textContent}`;
-        firstNumber = result;
+        secondNumber = parseFloat(newOp.textContent);
+        const result = operation(firstNumber, secondNumber, operator);
+        updatePrevOpText(`${firstNumber} ${operator} ${secondNumber} =`);
+        updateNewOpText(result);
+        firstNumber = NaN;
         secondNumber = NaN;
         operator = null;
-        newOp.textContent = result;
         isPlaceHolder = true;
     } 
 }
@@ -148,14 +156,14 @@ function operation(firstNum, secondNum, op){
 }
 
 numBtns.forEach((button) => {
-    button.addEventListener('click', addNumberToScreen);
+    button.addEventListener('click', onNumberClick);
 });
 
 operatorBtns.forEach((button) => {
     button.addEventListener('click', operatorClicked);
 });
 
-decimalBtn.addEventListener('click', addDecimalToScreen);
+decimalBtn.addEventListener('click', onDecimalClick);
 
 signBtn.addEventListener('click', addSignToScreen);
 
